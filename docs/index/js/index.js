9c30e30efb0ebf320d2391d4a3d5d91439e3b27f
Stephen Cheesley <34319886+SteveCheesley@users.noreply.github.com>
2018-01-03 21:45:44 +0000

    Adding working JS that retrieves the list of supported releases and builds the Javadoc links.

--- docs/index/js/index.js
+++ docs/index/js/index.js
@@ -1,14 +1,17 @@
 function buildReleases() {
-    var json = JSON.parse(releases);
+    $.getJSON( "https://logitopia.github.io/jmortar-core-test/supported_releases.json", {
+        format: "json"
+      })
+        .done(function( data ) {
+                var output = []
+                for (i=0; i < data.length; i++) {
+                    var href = "javadoc/" + data[i].replace(/\./g,'_') + "/index.html"
 
-    var output = []
-    for (i=0; i < json.length; i++) {
-        var href = "javadoc/" + json[i].replace(/\./g,'_') + "/index.html"
+                    if (i === 0) { output.push("<h6 class='dropdown-header'>Latest Release</h6>"); }
+                    output.push("<a class=\"dropdown-item\" href=\"" + href + "\">" + data[i] + "</a>");
+                    if (i == 0) { output.push("<div class='dropdown-divider'></div>") };
+                }
 
-        if (i === 0) { output.push("<h6 class='dropdown-header'>Latest Release</h6>"); }
-        output.push("<a class=\"dropdown-item\" href=\"" + href + "\">" + json[i] + "</a>");
-        if (i == 0) { output.push("<div class='dropdown-divider'></div>") };
-    }
-
-    document.getElementById('jdoc-dropdown').innerHTML += output.join('');
+                document.getElementById('jdoc-dropdown').innerHTML += output.join('');
+        });
 }
