e47eab1ff1f52c975a15db1df7a039c2fa5b88de
Stephen Cheesley <34319886+SteveCheesley@users.noreply.github.com>
2018-01-05 21:08:28 +0000

    Updating the developer docs so that they display the supported version links for the test report.

--- docs/index/js/index.js
+++ docs/index/js/index.js
@@ -1,5 +1,6 @@
-function buildReleases() {
-    $.getJSON( "https://logitopia.github.io/jmortar-core-test/supported_releases.json", {
+var supportedReleasesFilePath = "https://logitopia.github.io/jmortar-core-test/supported_releases.json"
+function buildJavadocReleases() {
+    $.getJSON( supportedReleasesFilePath, {
         format: "json"
       })
         .done(function( data ) {
@@ -13,5 +14,23 @@ function buildReleases() {
                 }
 
                 document.getElementById('jdoc-dropdown').innerHTML += output.join('');
+        });
+}
+
+function buildTestReportReleases() {
+    $.getJSON( supportedReleasesFilePath, {
+        format: "json"
+      })
+        .done(function( data ) {
+                var output = []
+                for (i=0; i < data.length; i++) {
+                    var href = "TestReport/" + data[i].replace(/\./g,'_') + "/index.html"
+
+                    if (i === 0) { output.push("<h6 class='dropdown-header'>Latest Release</h6>"); }
+                    output.push("<a class=\"dropdown-item\" href=\"" + href + "\">" + data[i] + "</a>");
+                    if (i == 0) { output.push("<div class='dropdown-divider'></div>") };
+                }
+
+                document.getElementById('testrep-dropdown').innerHTML += output.join('');
         });
 }
