428a734fe73e3ca58b6c75392f510947d595808b
Stephen Cheesley <34319886+SteveCheesley@users.noreply.github.com>
2018-01-09 22:23:15 +0000

    Updating the private test method exception to initialise with a cause.

--- src/main/java/com/logitopia/platform/core/test/exception/PrivateTestMethodException.java
+++ src/main/java/com/logitopia/platform/core/test/exception/PrivateTestMethodException.java
@@ -4,4 +4,13 @@ package com.logitopia.platform.core.test.exception;
  * An exception that is thrown when there is an issue calling a private test method.
  */
 public class PrivateTestMethodException extends Exception {
+
+    /**
+     * Create the exception siting the cause of the error.
+     *
+     * @param cause
+     */
+    public PrivateTestMethodException(Throwable cause) {
+        super(cause);
+    }
 }
