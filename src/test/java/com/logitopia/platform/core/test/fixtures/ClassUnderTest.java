35eefe48464f12ec9b36942c57ae724f14e434a4
Stephen Cheesley <34319886+SteveCheesley@users.noreply.github.com>
2018-01-09 22:37:26 +0000

    Adding a test subject class for unit testing against.

---
+++ src/test/java/com/logitopia/platform/core/test/fixtures/ClassUnderTest.java
@@ -0,0 +1,16 @@
+package com.logitopia.platform.core.test.fixtures;
+
+/**
+ * A test subject class.
+ */
+public class ClassUnderTest {
+
+    /**
+     * A private method that returns a pre-determined value for testing.
+     *
+     * @return A predetermined value (5)
+     */
+    private int getValue() {
+        return 5;
+    }
+}
