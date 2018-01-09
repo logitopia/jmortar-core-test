9db73e15b90b536136aa6ca12634547ea0dd989c
Stephen Cheesley <34319886+SteveCheesley@users.noreply.github.com>
2018-01-09 22:09:12 +0000

    Adding the private test method exception.

---
+++ src/main/java/com/logitopia/platform/core/test/exception/PrivateTestMethodException.java
@@ -0,0 +1,7 @@
+package com.logitopia.platform.core.test.exception;
+
+/**
+ * An exception that is thrown when there is an issue calling a private test method.
+ */
+public class PrivateTestMethodException extends Exception {
+}
