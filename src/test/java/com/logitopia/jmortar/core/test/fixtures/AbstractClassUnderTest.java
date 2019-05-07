5c2c3c3de390c3c4a18af82a2202a5aa234bf1b2
scheesley <stephen.cheesley@gmail.com>
2019-05-07 21:27:58 +0100

    Adding an abstract class to test with.

---
+++ src/test/java/com/logitopia/jmortar/core/test/fixtures/AbstractClassUnderTest.java
@@ -0,0 +1,10 @@
+package com.logitopia.jmortar.core.test.fixtures;
+
+/**
+ * An abstract test class.
+ */
+public class AbstractClassUnderTest {
+
+    private String abstractClassField = "abstractFieldUnderTest";
+
+}
